22021614744308c0137d05c4f7ad22c8a3bf76fb
Shridhar Suryanarayan <s4suryan@uwaterloo.ca>
2022-10-02 13:32:07 -0400

    working without rule 7

--- models/rule.js
+++ models/rule.js
@@ -18,46 +18,16 @@ class Rule {
     }
   }
 
-  apply(ogTxnTable) {
+  apply(txnTable) {
     // validate that the rule applies to the current transaction Table
-    if (!this.validate(ogTxnTable)) {
+    if (!this.validate(txnTable)) {
       console.log("Validation failed");
-      return null;
+      return false;
     }
     console.log("Validated");
-    var txnTable = cloneDeep(ogTxnTable);
     for (var mr of this.merchant_rules) {
-      // this condition only happens once
-      if (mr.merchant === "*") {
-        // add mr.amount points for each dollar remaining
-        // for all merchants in txn map
-        amount_to_dist = mr.amount;
-        for (var [key, value] of [...txnTable.tmap]) {
-          if (amount_to_dist <= 0) {
-            break;
-          }
-          for (var txn of value.txns) {
-            if (amount_to_dist <= 0) {
-              break;
-            }
-            if (txn.amount_cents === 0) {
-              continue;
-            }
-            if (txn.amount_cents <= amount_to_dist) {
-              amount_to_dist -= txn.amount_cents;
-              txn.points +=
-                (txn.amount_cents / mr.amount) * mr.weight * this.points;
-              txn.amount_cents = 0;
-            } else {
-              txn.amount_cents -= amount_to_dist;
-              txn.points +=
-                (amount_to_dist / mr.amount) * mr.weight * this.points;
-              amount_to_dist = 0;
-            }
-            txn.points = Math.round(txn.points);
-          }
-        }
-      } else {
+        // decrease cumulative amount in the table
+        txnTable.cumulative_amount -= mr.amount;
         // decrease applied promotion amount for the merchant
         txnTable.tmap.get(mr.merchant).total -= mr.amount;
         var amount_to_dist = mr.amount;
@@ -81,45 +51,31 @@ class Rule {
               (amount_to_dist / mr.amount) * mr.weight * this.points;
             amount_to_dist = 0;
           }
+          // TODO: manage split rounding
+          // i.e when 3 merchants have % split: eg 83.33, 33.34, 33.34
+          // round will not cumulatively add to full value
           txn.points = Math.round(txn.points);
         }
-      }
     }
     txnTable.total_points += this.points;
-    return txnTable;
+    return true;
   }
 
-  hasAtLeast(amount, txnTable) {
-    var total_amount_txnTable = 0;
 
-    for (var [key,value] of [...txnTable.tmap]) {
-      total_amount_txnTable += value.total;
-      if (total_amount_txnTable >= amount) {
-        return true;
-      }
-    }
-    return false;
-  }
 
   validate(txnTable) {
-    // if merchant '*' exists, that rule works for all
-    // as long as theres at least 1 merchant with the given mr.amount
-    var star_merchant = this.merchant_rules.find((e) => e.merchant === "*");
-
-    if (star_merchant && !this.hasAtLeast(star_merchant.amount, txnTable)) {
-      return false;
-    }
 
     // iterate through merchant_rules: MRi
     // if MRi in txnTable and txnTable[MRi.merchant] >= MRi.amount
     for (var mr of this.merchant_rules) {
-      if (mr.merchant === "*") {
-        continue;
-      }
+      // if (mr.merchant === "*") {
+      //   continue;
+      // }
       if (
         !(
           txnTable.tmap.has(mr.merchant) && txnTable.tmap.get(mr.merchant).total >= mr.amount
         )
+
       ) {
         return false;
       }
--- routes/api/rewards.js
+++ routes/api/rewards.js
@@ -3,51 +3,71 @@ var router = require("express").Router();
 const util = require("util");
 const MerchantRule = require("../../models/merchant_rule");
 const Rule = require("../../models/rule");
-const Combination = require("../../models/combination")
 router.post("/", validateData, rewardCalc);
 
 function rewardCalc(req, res) {
   var txnTable = generateTransactionTable(req.body);
-  var rules = generateRules();
-  
-  const comboGen = new Combination();
-  comboGen.findBestCombo(txnTable, rules);
-  console.log(`maxpts = ${comboGen.maxpts}`);
-  console.log(util.inspect(comboGen.maxtable, false, null, true));
-  // console.log(util.inspect(rules, false, null, true));
-  // for (let i = 0; i < rules.length; i++) {
-  //   var modified_table = rules[i].apply(txnTable);
-    // console.log(`Rule ${i+1}`);
-    // console.log(util.inspect(modified_table, false, null, true));
-  // }
-  // console.log("org_table");
-  // console.log(util.inspect(txnTable, false, null, true));
+  var rules = generateRules(); // Rules generated with calculated priority
+
+  console.log(util.inspect(txnTable, false, null, true));
+  for (let i = 0; i < rules.length; i++) {
+    var ret = true;
+    while (ret) {
+      ret = rules[i].apply(txnTable);
+    }
+  }
+
+  // applyRule7(txnTable);
+
+  console.log(util.inspect(txnTable, false, null, true));
   res.status(201).json({
-    data: req.body,
-    // data: arr,
+    data: {
+      total: txnTable.total_points,
+      txns: JSON.stringify(...txnTable.tmap),
+    },
   });
 }
 
-
-
-
-
+function applyRule7(txnTable) {
+  var amount_to_dist = Math.floor(txnTable.total_amount/100);
+  txnTable.total_points += amount_to_dist;
+  for (var [key, value] of [...txnTable.tmap]) {
+    if (amount_to_dist <= 0) {
+      break;
+    }
+    for (var txn of value.txns) {
+      if (amount_to_dist <= 0) {
+        break;
+      }
+      if (txn.amount_cents === 0) {
+        continue;
+      }
+      
+      amount_to_dist -= txn.amount_cents;
+      txn.points +=  txn.amount_cents
+      txn.amount_cents = 0;
+      
+    }
+  }
+}
 // {
 //   total_points = 0
 //   tmap:{
-  //   'sportcheck': {
-  //     total: 0,
-  //     txns: [t1,t2]
-  //   }
+//   'sportcheck': {
+//     total: 0,
+//     txns: [t1,t2]
+//   }
 //   }
 // }
 function generateTransactionTable(transactions) {
   var txnTable = {
-    total_points : 0,
-    tmap: new Map()
+    total_points: 0,
+    cumulative_amount: 0,
+    tmap: new Map(),
   };
   // iterate through input json and get a list of txn objects
   for (var key of Object.keys(transactions)) {
+    txnTable.cumulative_amount += transactions[key].amount_cents;
     var txn = new Transaction(
       key,
       transactions[key].date,
@@ -81,19 +101,6 @@ function generateRules() {
   var r1 = new Rule(r1_points, [mr1_1, mr1_2, mr1_3]);
   rules.push(r1);
 
-  // rule 2
-  var mr2_1 = new MerchantRule("sportcheck", 7500);
-  var mr2_2 = new MerchantRule("tim_hortons", 2500);
-  var r2_points = 300;
-  var r2 = new Rule(r2_points, [mr2_1, mr2_2]);
-  rules.push(r2);
-
-  // rule 3
-  var mr3_1 = new MerchantRule("sportcheck", 7500);
-  var r3_points = 200;
-  var r3 = new Rule(r3_points, [mr3_1]);
-  rules.push(r3);
-
   // rule 4
   var mr4_1 = new MerchantRule("sportcheck", 2500);
   var mr4_2 = new MerchantRule("tim_hortons", 1000);
@@ -102,12 +109,12 @@ function generateRules() {
   var r4 = new Rule(r4_points, [mr4_1, mr4_2, mr4_3]);
   rules.push(r4);
 
-  // rule 5
-  var mr5_1 = new MerchantRule("sportcheck", 2500);
-  var mr5_2 = new MerchantRule("tim_hortons", 1000);
-  var r5_points = 75;
-  var r5 = new Rule(r5_points, [mr5_1, mr5_2]);
-  rules.push(r5);
+  // rule 2
+  var mr2_1 = new MerchantRule("sportcheck", 7500);
+  var mr2_2 = new MerchantRule("tim_hortons", 2500);
+  var r2_points = 300;
+  var r2 = new Rule(r2_points, [mr2_1, mr2_2]);
+  rules.push(r2);
 
   // rule 6
   var mr6_1 = new MerchantRule("sportcheck", 2000);
@@ -115,7 +122,20 @@ function generateRules() {
   var r6 = new Rule(r6_points, [mr6_1]);
   rules.push(r6);
 
-  // rule 7
+  // rule 5
+  var mr5_1 = new MerchantRule("sportcheck", 2500);
+  var mr5_2 = new MerchantRule("tim_hortons", 1000);
+  var r5_points = 75;
+  var r5 = new Rule(r5_points, [mr5_1, mr5_2]);
+  rules.push(r5);
+
+  // rule 3
+  var mr3_1 = new MerchantRule("sportcheck", 7500);
+  var r3_points = 200;
+  var r3 = new Rule(r3_points, [mr3_1]);
+  rules.push(r3);
+
+  // // rule 7
   // var mr7_1 = new MerchantRule("*", 100);
   // var r7_points = 1;
   // var r7 = new Rule(r7_points, [mr7_1]);
@@ -131,6 +151,5 @@ function validateData(req, res, next) {
   // create a seperate func to parse transactions in required datastructure
   next();
 }
-
 
 module.exports = router;
